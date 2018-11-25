012f06e3e9ce02b533f9791ea3aa0b1ac71b04ea
Peter <pvroom@convergence-us.com>
2018-11-24 23:13:28 -0500

    Version #1
    Resource files for splash screen and icon. Added issues to Issues page.

---
+++ Flyin1/Flyin1/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Flyin1
+//
+//  Created by Peter Vroom on 11/23/18.
+//  Copyright © 2018 Convergence. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ Flyin1/Flyin1Tests/Flyin1Tests.m
@@ -0,0 +1,37 @@
+//
+//  Flyin1Tests.m
+//  Flyin1Tests
+//
+//  Created by Peter Vroom on 11/23/18.
+//  Copyright © 2018 Convergence. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Flyin1Tests : XCTestCase
+
+@end
+
+@implementation Flyin1Tests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
