da295e20c52d7ea4fc04d1952676d178f49ef423
Peter <pvroom@convergence-us.com>
2018-12-01 01:21:12 -0500

    Version 15

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,8 +1,8 @@
 import { Component, ViewChild } from '@angular/core';
 import { NavController, Platform } from 'ionic-angular';
-//import { LeafletDirective } from '@asymmetrik/ngx-leaflet/dist';
+import { LeafletDirective } from '@asymmetrik/ngx-leaflet/dist';
 
-//import * as L from "leaflet";
+import * as L from "leaflet";
 
 declare var google: any;
 
@@ -12,10 +12,10 @@ declare var google: any;
 })
 export class MapPage {
 
-	// Leaflet mapping variables
-	//myMap2: any;
-	//myMap3: any;
-	//myMap4: any;
+	 Leaflet;
+	myMap2: any;
+	myMap3: any;
+    myMap4: any;
 
 	constructor(public navCtrl: NavController, 
 				public platform: Platform) {
@@ -25,7 +25,7 @@ export class MapPage {
 
 	ngOnInit() {
 		
-		/*
+		
 		// -----------------
 		// Show Second Floor
 		// -----------------
@@ -37,7 +37,7 @@ export class MapPage {
 		});
 
 		var bounds = L.latLngBounds([0, 0], [1200, 1700]);    // Normally 1000, 1000; stretched to 2000,1000 for AACD 2017
-		var image = L.imageOverlay('assets/img/ausJW2a.png', bounds, {
+		var image = L.imageOverlay('assets/img/assets/img/capitol_campus_map.jpg', bounds, {
 			attribution: 'Convergence'
 		}).addTo(this.myMap2);
 
@@ -55,7 +55,7 @@ export class MapPage {
 		});
 
 		var bounds = L.latLngBounds([0, 0], [1200, 1700]);    // Normally 1000, 1000; stretched to 2000,1000 for AACD 2017
-		var image = L.imageOverlay('assets/img/ausJW3a.png', bounds, {
+		var image = L.imageOverlay('assets/img/assets/img/house-inside.jpg', bounds, {
 			attribution: 'Convergence'
 		}).addTo(this.myMap3);
 
@@ -73,13 +73,13 @@ export class MapPage {
 		});
 
 		var bounds = L.latLngBounds([0, 0], [1200, 1700]);    // Normally 1000, 1000; stretched to 2000,1000 for AACD 2017
-		var image = L.imageOverlay('assets/img/ausJW4a.png', bounds, {
+		var image = L.imageOverlay('assets/img/senate-inside.jpg', bounds, {
 			attribution: 'Convergence'
 		}).addTo(this.myMap4);
 
 		this.myMap4.fitBounds(bounds);
 		this.myMap4.setMaxBounds(bounds);
-		*/
+		
 	}
 	
 }
