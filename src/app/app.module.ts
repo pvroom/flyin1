6b535fe4a8350803a5416868b7c1eee42164649e
Peter <pvroom@convergence-us.com>
2018-12-02 01:37:32 -0500

    Version 22

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -62,9 +62,6 @@ import { SurveyPage } from '../pages/survey/survey';
 // Temporary Support Pages
 //import { FloorplanMappingPage } from '../pages/floorplanmapping/floorplanmapping';
 
-//Pro.init('cc0b9cf2', {
-  //appVersion: '4.2.3'
-//})
 
 @Injectable()
 export class MyErrorHandler implements ErrorHandler {
